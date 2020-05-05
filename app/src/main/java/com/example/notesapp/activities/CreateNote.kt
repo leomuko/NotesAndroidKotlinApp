42e95d9278fd5d7a7bb28176a716ccab198c18f1
leomuko <kezzieleo@gmail.com>
2020-05-05 17:05:12 +0300

    Modify Create Note Activity

--- app/src/main/java/com/example/notesapp/activities/CreateNote.kt
+++ app/src/main/java/com/example/notesapp/activities/CreateNote.kt
@@ -2,9 +2,12 @@ package com.example.notesapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.notesapp.R
 import com.example.notesapp.databinding.ActivityCreateNoteBinding
+import com.example.notesapp.helpers.sampleData
+import com.example.notesapp.models.NotesData
 
 class CreateNote : AppCompatActivity() {
     private lateinit var binding: ActivityCreateNoteBinding
@@ -14,5 +17,20 @@ class CreateNote : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_note)
 
 
+
+        binding.saveButton.setOnClickListener {
+            val newNote = NotesData()
+            newNote.title = binding.noteTitle.text.toString()
+            newNote.noteContent = binding.noteContent.text.toString()
+            sampleData.addNote(newNote)
+            finish()
+        }
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
