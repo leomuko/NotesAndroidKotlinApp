73b0318e3a7b82291f91e499a4379ac56c7ceb10
leomuko <kezzieleo@gmail.com>
2020-05-05 11:39:02 +0300

    Recieve data through intent

--- app/src/main/java/com/example/notesapp/activities/NoteDetail.kt
+++ app/src/main/java/com/example/notesapp/activities/NoteDetail.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.notesapp.R
 import com.example.notesapp.databinding.ActivityNoteDetailBinding
+import com.example.notesapp.models.NotesData
 
 class NoteDetail : AppCompatActivity() {
 
@@ -12,5 +13,16 @@ class NoteDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_note_detail)
+
+        var myData : NotesData = intent.getParcelableExtra("notes")
+        binding.theNote.text = myData.title
+        binding.noteDetail.text = myData.noteContent
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
